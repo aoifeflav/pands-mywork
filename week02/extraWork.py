6811afbc4a09a5c137a912ff162cc9862eccddd8
Aoife Flavin <G00439331@atu.ie>
2024-02-13 16:11:59 +0000

    Lab wk 2 extra questions

---
+++ week02/extraWork.py
@@ -0,0 +1,9 @@
+# extraWork.py
+# This contains the last 2 question of the Week 2 lab
+# Author: Aoife Flavin
+
+sum = 21-4
+print(sum)
+
+check = 2==3
+print(check)
---
+++ week02/nameAndAge.py
@@ -0,0 +1,8 @@
+# nameAndAge.py
+# This program states your name and age
+# Lab week 2
+# Author: Aoife Flavin
+
+name = input("What is your name?")
+age = str(input("How old are you?"))
+print("Hello " + name +",\t your age is " + age +".")
